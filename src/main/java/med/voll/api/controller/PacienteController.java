02aed4a1f48a41161584eebcc340d6de547a3447
jackvadderr <jacjacckck@gmail.com>
2022-12-20 19:33:35 -0400

    Finalizando PUT e DEL de pacientes.

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,11 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosAtualizacaoMedico;
-import med.voll.api.paciente.DadosCadastroPaciente;
-import med.voll.api.paciente.DadosListagemPacientes;
-import med.voll.api.paciente.Paciente;
-import med.voll.api.paciente.PacienteRepository;
+import med.voll.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -26,8 +22,21 @@ public class PacienteController {
 
     @GetMapping
     public Page<DadosListagemPacientes> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
-        return repository.findAll(paginacao).map(DadosListagemPacientes::new);
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemPacientes::new);
     }
 
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados){
+        var paciente = repository.getReferenceById(dados.id());
+        paciente.atualizarInformacoes(dados);
+    }
 
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        //repository.deleteById(id);
+        var paciente = repository.getReferenceById(id);
+        paciente.excluir();
+    }
 }
--- src/main/java/med/voll/api/medico/DadosListagemMedicos.java
+++ src/main/java/med/voll/api/medico/DadosListagemMedicos.java
@@ -1,5 +1,7 @@
 package med.voll.api.medico;
 
+import med.voll.api.paciente.Paciente;
+
 public record DadosListagemMedicos(Long id, String nome, String email, String crw, Especialidade especialidade) {
     public DadosListagemMedicos(Medico medico){
         this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrw(), medico.getEspecialidade());
